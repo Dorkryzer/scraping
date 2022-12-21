54857fe474aee6a7d38675b0f36815c1a40f7740
Dor kryzer <dorkryzer@gmail.com>
2022-12-21 10:08:36 +0200

    adding comments
    Signed-off-by: Dor kryzer <dorkryzer@gmail.com>

--- scraper.py
+++ scraper.py
@@ -3,13 +3,19 @@ import bs4
 import requests
 
 
+""" this script will take player IDs from dotabuff.com and compare the total 
+amount of kills in 1 match and will print all of them and also will
+print who had the most kills in 1 match """
+
+
+
 
 if __name__ == '__main__':
     players_list = [212709014,70388657,84584373]
-    heightest = ['',0]
+    heigh_test = ['',0]
 
     def kill_comparer(lister):
-        global heightest
+        global heigh_test
         for player in lister:
             url ='https://www.dotabuff.com/players/'+ str(player)+'/records'
             header = {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'}
@@ -19,10 +25,10 @@ if __name__ == '__main__':
             ans = int(article[1].text)
             name = doc.find('div',class_='header-content-title').text[:-7] 
             print(f'{name} has a record of {ans} kills in one match')
-            if int(ans) > heightest[1]:
-                heightest = [name,ans]
+            if int(ans) > heigh_test[1]:
+                heigh_test = [name,ans]
             
     kill_comparer(players_list)
-    print(f'{heightest[0]} has the most above all with {heightest[1]}')
+    print(f'{heigh_test[0]} has the most above all with {heigh_test[1]}')
     
     
